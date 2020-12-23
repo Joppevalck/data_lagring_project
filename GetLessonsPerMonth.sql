7ad1ba69bf5664c22b18ae1fa8cc2d7fec888e11
Johannes Valck <johannesvalck@gmail.com>
2020-12-23 18:55:14 +0100

    woopsie

---
+++ GetLessonsPerMonth.sql
@@ -0,0 +1,14 @@
+SELECT COALESCE(q.month, 'ALL') AS "Month",
+    COALESCE(q.type, 'ALL') AS "Type of Lesson",
+    q.count AS "Number of Lessons"
+FROM (
+    SELECT TO_CHAR(m.start_time, 'Month') AS month, 
+        m.type AS type,
+        COUNT(*) AS count,
+        MIN(m.start_time) AS time
+    FROM music_lesson AS m
+    GROUP BY 
+        ROLLUP (month),
+        ROLLUP (m.type)
+    ) AS q
+ORDER BY q.time;
