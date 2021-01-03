a2e1a64f789090cf6ba67d5ae7f96d352f35c64c
Johannes Valck <johannesvalck@gmail.com>
2021-01-03 16:59:09 +0100

    deleted tests

--- src/sql/test.sql
+++
@@ -1,5 +0,0 @@
-SELECT r.rental_id, ri.name, r.rental_date, r.return_date
-FROM rental AS r
-NATURAL LEFT OUTER JOIN rental_instrument AS ri
-WHERE r.student_id = 2 
-AND r.terminated = false;
--- src/sql/test2.sql
+++
@@ -1,9 +0,0 @@
-SELECT COALESCE(q.terminated, true)
-FROM (
-    SELECT ri.rental_instrument_id, r.terminated
-    FROM rental_instrument AS ri 
-    NATURAL LEFT OUTER JOIN rental AS r
-    ORDER BY r.rental_date DESC
-) AS q
-WHERE q.rental_instrument_id = 9
-LIMIT 1;
